db3fa2c85be55912660c0f9f8277366f601ef6e1
prince954 <akprince0268@gmail.com>
2024-03-26 11:22:45 +0600

    all members page

---
+++ pages/all_members.php
@@ -0,0 +1,92 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Add Mass Member</title>
+      <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+
+      <style>
+         .error{color: #FF0000;}
+         .success{color:green;}
+         .start{display:none}
+      </style>
+
+   </head>
+   <body>
+      <div class="container pt-5 ">
+         <!-- ======================  From  ============================= -->
+         <form action="" method="POST">
+            <div class='container py-5'>
+               <h2 class="text-center pb-3">All Members</h2>
+               <div class="row g-3 ">
+                    <div class="col-md-4">
+                        <div class="card" style="">
+                            <img src="../assets/image/profile_image/profile_1.png" class="card-img-top" alt="...">
+                            <div class="card-body d-flex flex-column ">
+                                <h5 class="card-title">Name :</h5>
+                                <h5 class="card-title">Email :</h5>
+                                <h5 class="card-title">Number :</h5>
+                                <a href="#" class="btn btn-primary mb-2 ">Manager</a>
+                                <a href="#" class="btn btn-primary">Remove Member</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="">
+                            <img src="../assets/image/profile_image/profile_1.png" class="card-img-top" alt="...">
+                            <div class="card-body d-flex flex-column ">
+                                <h5 class="card-title">Name :</h5>
+                                <h5 class="card-title">Email :</h5>
+                                <h5 class="card-title">Number :</h5>
+                                <a href="#" class="btn btn-primary mb-2 ">Manager</a>
+                                <a href="#" class="btn btn-primary">Remove Member</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="">
+                            <img src="../assets/image/profile_image/profile_1.png" class="card-img-top" alt="...">
+                            <div class="card-body d-flex flex-column ">
+                                <h5 class="card-title">Name :</h5>
+                                <h5 class="card-title">Email :</h5>
+                                <h5 class="card-title">Number :</h5>
+                                <a href="#" class="btn btn-primary mb-2 ">Manager</a>
+                                <a href="#" class="btn btn-primary">Remove Member</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="">
+                            <img src="../assets/image/profile_image/profile_1.png" class="card-img-top" alt="...">
+                            <div class="card-body d-flex flex-column ">
+                                <h5 class="card-title">Name :</h5>
+                                <h5 class="card-title">Email :</h5>
+                                <h5 class="card-title">Number :</h5>
+                                <a href="#" class="btn btn-primary mb-2 ">Manager</a>
+                                <a href="#" class="btn btn-primary">Remove Member</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="">
+                            <img src="../assets/image/profile_image/profile_1.png" class="card-img-top" alt="...">
+                            <div class="card-body d-flex flex-column ">
+                                <h5 class="card-title">Name :</h5>
+                                <h5 class="card-title">Email :</h5>
+                                <h5 class="card-title">Number :</h5>
+                                <a href="#" class="btn btn-primary mb-2 ">Manager</a>
+                                <a href="#" class="btn btn-primary">Remove Member</a>
+                            </div>
+                        </div>
+                    </div>
+               </div>
+            </div>
+      </div>
+      </form>
+      </div>
+      <script src="../assets/js/bootstrap.bundle.min.js"></script>
+   </body>
+</html>
